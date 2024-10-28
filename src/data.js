1a74f2a3bae01d4cbebea8d12416ba5298bfc4f6
veloz-web <adelorie@silveredge-gs.com>
2024-10-28 16:12:57 -0400

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const quarterbacks = [
+  { name: 'Quarterback 1', points: 0 },
+  { name: 'Quarterback 2', points: 0 },
+  { name: 'Quarterback 3', points: 0 },
+];
+
+export const opponents = [
+  { name: 'Opponent 1', selections: ['Quarterback 2', 'Quarterback 1', 'Quarterback 3', /* more */] },
+  { name: 'Opponent 2', selections: ['Quarterback 1', 'Quarterback 3', 'Quarterback 2', /* more */] },
+];
